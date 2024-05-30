be03dcb8157564b02d04b6f88bb5db4522a46ee5
Leonel Ortega <leonel.ortega@tucambista.com>
2024-05-30 18:58:49 -0500

    add CardController

--- smartsproutbackend/src/main/java/com/upc/smartsproutbackend/models/User.java
+++ smartsproutbackend/src/main/java/com/upc/smartsproutbackend/models/User.java
@@ -53,6 +53,10 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CropField> cropFields;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Card> cards;
+
     @Enumerated(EnumType.STRING)
     private Roles role;
 
